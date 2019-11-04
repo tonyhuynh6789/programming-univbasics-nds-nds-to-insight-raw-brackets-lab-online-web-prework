4722b1e1761ab4845f5ced76963df07599e0a040
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-04 00:53:40 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,15 +1,33 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
-
+require "pry"
 def directors_totals(nds)
+
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  result = {
-  }
-  #
+
+result = {
+}  
+
+
+row_index = 0
+#binding.pry
+while row_index < directors_database.count do
+inner_index = 0
+total = 0 
+  while inner_index <directors_database[row_index][:movies].count 
+  total += directors_database[row_index][:movies][inner_index][:worldwide_gross]
+  inner_index += 1
+  end 
+name = directors_database[row_index][:name]
+result[name] = total
+row_index += 1
+end   
+result 
+end 
   # Use loops, variables and the accessing method, [], to loop through the NDS
   # and total up all the
   # ...
@@ -18,5 +36,5 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
-end
+  
+
